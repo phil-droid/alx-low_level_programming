8fa1465d0a899670bfa10321b22b6268641138b1
phil-droid <akkoli058@gmail.com>
2022-09-28 00:14:01 -0700

    function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,17 +9,17 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-		if (y == 0)
-		{	
-													return (1);
-												}
-			if (x > 0 || x < 0)
-													{
-														return (x * _pow_recursion(x, y - 1));
-													}
-		return (0);
+if (y < 0)
+{
+	return (-1);
+}
+if (y == 0)
+{	
+	return (1);
+}
+if (x > 0 || x < 0)
+{
+	return (x * _pow_recursion(x, y - 1));
+}
+return (0);
 }
