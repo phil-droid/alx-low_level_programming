9ccbe9301eb35c43da007f612b5d197a39022fe7
phil-droid <akkoli058@gmail.com>
2022-10-26 11:37:31 -0700

    lists.h header file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,20 +8,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		char *p;
-			unsigned int i, d;
+	char *p;
+	unsigned int i, d;
 
-				d = nmemb * size;
-					if (nmemb <= 0 || size <= 0)
-								return (NULL);
+	d = nmemb * size;
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 
-						p = malloc(d);
-							if (p == NULL)
-										return (NULL);
+	p = malloc(d);
+	if (p == NULL)
+		return (NULL);
 
-								for (i = 0; i < (d); i++)
-										{
-													p[i] = 0;
-														}
-									return (p);
+	for (i = 0; i < (d); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
 }
